148f01bae6b22a01b417cb8cbe5d18bf32d7da87
mkalashnikau <kalashnikovgomel@gmail.com>
2022-04-14 11:24:13 +0300

    Changed extension for pages to html

--- booking-app/pkg/handlers/handlers.go
+++ booking-app/pkg/handlers/handlers.go
@@ -9,10 +9,10 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.html")
 }
