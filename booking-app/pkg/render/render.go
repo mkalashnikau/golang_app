9053869f4e6c50ff791a730df3373360a030f9cd
mkalashnikau <kalashnikovgomel@gmail.com>
2022-04-14 16:09:30 +0300

    Updated render module

--- booking-app/pkg/render/render.go
+++ booking-app/pkg/render/render.go
@@ -4,13 +4,59 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
+// FuncMap is a map of functions that we can use in a template
+var functions = template.FuncMap{}
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	_, err := RenderTemplateTest(w, tmpl)
+	if err != nil {
+		fmt.Println("error getting template cache:", err)
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		// Define if we need to process the template or leave as is
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
